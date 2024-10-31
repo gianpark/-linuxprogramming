51da28811a8d84f0778a300861647949d6ed715c
gianpark <gianpark54@gmail.com>
2024-10-31 10:48:56 +0900

    Delete cpp7-3/cpptest1 directory

--- cpp7-3/cpptest1/main.cpp
+++
@@ -1,8 +0,0 @@
-#include"mylib.hpp"
-int main() {
-    complex x(2, 3), y(-5, 10), sum;
-    sum = x + y;
-    cout << "두 복소수의 합은";
-    sum.show();
-    return 0;
-}
--- cpp7-3/cpptest1/mylib.cpp
+++
@@ -1,9 +0,0 @@
-#include"mylib.hpp"
-void complex::show() {
-    cout << real << "+" << img << "j" << endl;
-}
-complex& complex::operator+(complex op2) {
-    this->real = this->real + op2.real;
-    this->img = this->img + op2.img;
-    return *this;
-}
