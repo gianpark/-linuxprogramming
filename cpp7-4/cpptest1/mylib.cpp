26fada6cc788fbb3bef9b056807a0ade4fd58017
gianpark <gianpark54@gmail.com>
2024-11-04 15:53:46 +0900

    Update mylib.cpp

--- cpp7-4/cpptest1/mylib.cpp
+++ cpp7-4/cpptest1/mylib.cpp
@@ -9,7 +9,7 @@ complex& complex::operator++() { //++전위 연산자 함수 정의
 }
 complex complex::operator--(int x) { //--전위 연산자 함수 정의
     complex tmp=*this;
-   real--; //실수증가
-   img--; //허수증가
+   real--; //허수감소
+   img--; //허수감소
    return tmp; //변경된 결과값 리턴
 }
