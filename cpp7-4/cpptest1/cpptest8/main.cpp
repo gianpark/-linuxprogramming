5f80bb1019a125f082608d0aa31ffb6df3eaf746
gianpark <gianpark54@gmail.com>
2024-11-04 20:07:54 +0900

    Delete cpp7-4/cpptest1 directory

--- cpp7-4/cpptest1/cpptest8/main.cpp
+++
@@ -1,5 +0,0 @@
-#include"mylib.hpp"
-int main(){
-    Book book("벼룩시장",0,50);
-    if(!book)cout<<"공짜다"<<endl;
-}
--- cpp7-4/cpptest1/cpptest8/mylib.cpp
+++
@@ -1,8 +0,0 @@
-#include"mylib.hpp" //헤더파일 호출
-
-void Book::show() {
-    cout<<title<<' '<<price<<"원"<<pages<<"페이지"<<endl;
-}
-bool Book::operator!(){ // !연산자 정의
-    if(price==0)return true;
-}
