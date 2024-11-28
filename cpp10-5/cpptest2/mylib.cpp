335c7924b459e37705c403f9fc3d824fd3fa61be
gianpark <gianpark54@gmail.com>
2024-11-28 20:46:29 +0900

    Add files via upload

--- cpp10-5/cpptest2/mylib.cpp
+++ cpp10-5/cpptest2/mylib.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main() {
     int score[3][3];
     int sum;
-    double avg,big=0;
+    double avg,big=0,yeol;
+    vector<int> a;
     for (int i = 0; i < 3; i++)
     {
         cout << i+1 <<"번째 학생의 국어,영어,수학 성적을 입력:";
@@ -16,8 +18,11 @@ int main() {
         for (int j = 0; j < 3; j++)
             sum += score[i][j];
             avg = sum / 3.0;
-            if(avg>big)big=avg;
+            if(avg>big){
+                big=avg;
+                yeol=i+1;
+            }
     }
-    cout<<"최우수 학생은 1번째 학생이고 평균점수는"<<big<<"점이다."<<endl;
+    cout<<"최우수 학생은 "<<yeol<<"번째 학생이고 평균점수는"<<big<<"점이다."<<endl;
     return 0;
 }
