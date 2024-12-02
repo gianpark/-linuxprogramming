8ac4641fc3d8f7ce39e22ba8d34671d80a571874
gianpark <gianpark54@gmail.com>
2024-12-02 15:38:31 +0900

    Update mylib.cpp

--- cpp10-6/cpptest4/mylib.cpp
+++ cpp10-6/cpptest4/mylib.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
     vector<vector<int>> x={{-5,2,35},{-20,5,100},{-75,5,-25}};
     cout<<"수정행렬"<<endl;
-   for (int i = 0; i < 3; i++)
+   for (int i = 0; i < x.size(); i++)
     {
-        for (int j = 0; j < 3; j++){
+        for (int j = 0; j < x[i].size(); j++){
             if(x.at(i).at(j)>0){
                 x.at(i).at(j)=255;
             }
